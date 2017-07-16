b53cea559957284aa2b98ab37ff4d45ae766b521
barnrang <barnrang@gmail.com>
2017-07-16 20:09:07 +0900

    add search box

--- script/main.js
+++ script/main.js
@@ -2,15 +2,54 @@ String.prototype.isEmpty = function() {
     return (this.length === 0 || !this.trim());
 };
 
+function checkSubString(sub, str){
+  var lensub = sub.length;
+  var lenstr = str.length;
+  str = str.toLowerCase();
+  sub = sub.toLowerCase();
+  var i = 0, j = 0;
+  if(lensub > lenstr) return false;
+  for(i = 0; i < lenstr; i++){
+    var chr = str[i];
+    if(chr == sub[j]){
+      j++;
+    }
+    if(j == lensub){
+      return true;
+    }
+  }
+  return false;
+}
+
 
 $(document).ready(function(){
   alert("js success")
+  //Search box
+
+  $('.searchBox').keyup(function(event){
+    $('.searchResult').empty();
+    var list = $(".job");
+    console.log(list[0].innerText);
+    var i = 0;
+    var sub = $(this).val();
+    for(;i < list.length; i++){
+      if(checkSubString(sub, list[i].innerText)){
+        $('.searchResult').append(list[i].innerText + "<br>");
+      }
+    }
+    /*if(checkSubString($(this))){
+
+    }*/
+    /*console.log("key has been pressed " + pressCount + " times"
+  + "by key" + String.fromCharCode(event.which));*/
+  })
+
   $('#target').on('submit', function(e) {
     var text = $('#todoname').val();
     var tmp = text;
     //$('#todolist').append(text+"<br>");
     if(!text.isEmpty()){
-      $('#todolist').append("<tr><th>" + text + "</th><th>"
+      $('#todolist').append("<tr><th class=\"job\">" + text + "</th><th>"
     + "<input type=\"checkbox\" class=\"checkbox-regular\">" + "</tr>");
       $('#todoname').val("");
     }
