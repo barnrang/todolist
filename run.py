e63c4c88790e7d3b708baf825faa103454ffb716
Barnrang <barnrang@gmail.com>
2017-07-20 22:27:29 +0900

    Add local storage

--- run.py
+++ run.py
@@ -4,10 +4,12 @@ from flask import Flask, jsonify, render_template, request, url_for
 #from flask_jsglue import JSGlue
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def index():
-    return render_template("index.html")
-
-@app.route('/folder')
+    if request.method == "GET":
+        return render_template("index.html")
+        
+@app.route('/folder', methods = ['GET','POST'])
 def folder():
-    return render_template("folder.html")
+    if request.method == "GET":
+        return render_template("folder.html")
--- static/script/main.js
+++ static/script/main.js
@@ -25,6 +25,21 @@ function checkSubString(sub, str){
 $(document).ready(function(){
   alert("js success")
   //Search box
+  console.log("test");
+  var lists = {};
+  if(localStorage.tasklist){
+    console.log(JSON.parse(localStorage.tasklist));
+    var lists = JSON.parse(localStorage.tasklist);
+    //var i = 0;
+    var table = $('#todolist')
+    for(var key in lists){
+      console.log(key);
+      table.append("<tr><th class=\"job\">" + key + "</th><th>"
+    + "<input type=\"checkbox\" class=\"checkbox-regular\">" + "</tr>")
+    }
+  }else{
+    console.log("not found");
+  }
 
   $('.searchBox').keyup(function(event){
     $('.searchResult').empty();
@@ -47,6 +62,18 @@ $(document).ready(function(){
   $('#target').on('submit', function(e) {
     var text = $('#todoname').val();
     var tmp = text;
+    if(typeof(Storage) !== "undefined"){
+      if(localStorage.tasklist){
+        lists[text] = 1;
+        localStorage.tasklist = JSON.stringify(lists);
+      }
+      else{
+        var listTemp = {text:1};
+        localStorage.tasklist = JSON.stringify(listTemp);
+      }
+    }else{
+      alert("your Browser not support lol")
+    }
     //$('#todolist').append(text+"<br>");
     if(!text.isEmpty()){
       $('#todolist').append("<tr><th class=\"job\">" + text + "</th><th>"
@@ -65,10 +92,16 @@ $(document).ready(function(){
   $('#delete').click(function(){
     var tick = $(".checkbox-regular");
     var job = $("tr");
+    var jobName = $('.job');
     var i;
     for(i = 0; i < tick.length; i++){
       if(tick[i].checked){
+        console.log(jobName[i]);
+        var text = jobName[i].innerText;
+        console.log(text)
         job[i+1].remove();
+        delete lists[text];
+        localStorage.tasklist = JSON.stringify(lists);
       }
     }
   });
